e2712ae79b6992eac2a8c0e5443ee9701a032d1c
Levin <schumicx@gmial.com>
2016-07-19 10:20:11 +0800

    debug

--- app/src/main/java/com/xyt/hwms/ui/AffirmItemsActivity.java
+++ app/src/main/java/com/xyt/hwms/ui/AffirmItemsActivity.java
@@ -10,7 +10,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +25,7 @@ import com.xyt.hwms.bean.TransferDetail;
 import com.xyt.hwms.bean.User;
 import com.xyt.hwms.support.utils.ApplicationController;
 import com.xyt.hwms.support.utils.Constants;
+import com.xyt.hwms.support.utils.DateUtils;
 import com.xyt.hwms.support.utils.PreferencesUtils;
 
 import java.util.ArrayList;
@@ -32,26 +35,32 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnItemClick;
 
-public class AffirmItemsActivity extends BaseActivity {
+public class AffirmItemsActivity extends BaseActivity implements View.OnClickListener {
 
     public CacheWasteDialogFragment cacheWasteDialogFragment;
     @BindView(R.id.listview)
     ListView listview;
     @NotEmpty(trim = true, message = "请输入操作人账号")
     EditText operator;
-    @NotEmpty(trim = true, message = "请输入接收人账号")
+    //    @NotEmpty(trim = true, message = "请输入接收人账号")
     EditText receiver;
     private TextView total;
     private TextView verified;
     private TextView unverified;
     private TextView back;
+    private LinearLayout totalLayout;
+    private LinearLayout verifiedLayout;
+    private LinearLayout unverifiedLayout;
+    private LinearLayout backLayout;
     private List<TransferDetail> list = new ArrayList<>();
+    private List<TransferDetail> listData = new ArrayList<>();
     private AffirmItemsAdapter affirmItemsAdapter;
     private int applyIndex;
     private int totalNum;
     private int verifiedNum;
     private int unverifiedNum;
     private int backNum;
+    private String reasonArray[];
 
     @OnItemClick(R.id.listview)
     public void onItemClick(int position) {
@@ -69,19 +78,30 @@ public class AffirmItemsActivity extends BaseActivity {
 
         applyIndex = getIntent().getIntExtra("position", 0);
         list.addAll(Constants.AFFIRM_LIST.getCollection().get(applyIndex).getDetail());
+        listData = Constants.AFFIRM_LIST.getCollection().get(applyIndex).getDetail();
+        reasonArray = getResources().getStringArray(R.array.reason);
 
         View head = getLayoutInflater().inflate(R.layout.list_head_affirm_items, null);
         total = (TextView) head.findViewById(R.id.total);
         verified = (TextView) head.findViewById(R.id.verified);
         unverified = (TextView) head.findViewById(R.id.unverified);
         back = (TextView) head.findViewById(R.id.back);
+        totalLayout = (LinearLayout) head.findViewById(R.id.total_layout);
+        verifiedLayout = (LinearLayout) head.findViewById(R.id.verified_layout);
+        unverifiedLayout = (LinearLayout) head.findViewById(R.id.unverified_layout);
+        backLayout = (LinearLayout) head.findViewById(R.id.back_layout);
         operator = (EditText) head.findViewById(R.id.operator);
         receiver = (EditText) head.findViewById(R.id.receiver);
         TextInputLayout til = (TextInputLayout) head.findViewById(R.id.til);
 
+        totalLayout.setOnClickListener(this);
+        verifiedLayout.setOnClickListener(this);
+        unverifiedLayout.setOnClickListener(this);
+        backLayout.setOnClickListener(this);
+
         totalNum = list.size();
         total.setText("" + totalNum);
-        if (TextUtils.isEmpty(Constants.AFFIRM_LIST.getCollection().get(applyIndex).getOperator()) || Constants.TRANSFER_TYPE_INNER.equals(getIntent().getStringExtra("type"))) {
+        if (TextUtils.isEmpty(Constants.AFFIRM_LIST.getCollection().get(applyIndex).getOperator()) && Constants.TRANSFER_TYPE_INNER.equals(getIntent().getStringExtra("type"))) {
             operator.setText(new Gson().fromJson(PreferencesUtils.getString(ApplicationController.getInstance(), "user"), User.class).getAccount());
             Constants.AFFIRM_LIST.getCollection().get(applyIndex).setOperator(operator.getText().toString().trim());
 //            PreferencesUtils.putString(context, "affirm", new Gson().toJson(Constants.AFFIRM_LIST));
@@ -149,6 +169,89 @@ public class AffirmItemsActivity extends BaseActivity {
     }
 
     @Override
+    public void onClick(final View view) {
+        PopupMenu popup = new PopupMenu(context, view);
+        popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (view.getId()) {
+                    case R.id.total_layout:
+                        if (item.getItemId() == R.id.allPass) {
+                            for (TransferDetail detail : listData) {
+                                detail.setStatus(Constants.WASTE_PASS);
+                                detail.setBack_reason(null);
+                                detail.setBack_reason_index(null);
+                                detail.setTransfer_time(DateUtils.getCurrentTime1());
+                            }
+                        } else {
+                            for (TransferDetail detail : listData) {
+                                detail.setStatus(Constants.WASTE_BACK);
+                                detail.setContainer_label_code(null);
+                                detail.setBack_reason(reasonArray[reasonArray.length - 1]);
+                                detail.setBack_reason_index(String.valueOf(reasonArray.length - 1));
+                                detail.setTransfer_time(DateUtils.getCurrentTime1());
+                            }
+                        }
+                        break;
+                    case R.id.verified_layout:
+                        if (item.getItemId() == R.id.allBack) {
+                            for (TransferDetail detail : listData) {
+                                if (Constants.WASTE_PASS.equals(detail.getStatus())) {
+                                    detail.setStatus(Constants.WASTE_BACK);
+                                    detail.setContainer_label_code(null);
+                                    detail.setBack_reason(reasonArray[reasonArray.length - 1]);
+                                    detail.setBack_reason_index(String.valueOf(reasonArray.length - 1));
+                                    detail.setTransfer_time(DateUtils.getCurrentTime1());
+                                }
+                            }
+                        }
+                        break;
+                    case R.id.unverified_layout:
+                        if (item.getItemId() == R.id.allPass) {
+                            for (TransferDetail detail : listData) {
+                                if (!Constants.WASTE_PASS.equals(detail.getStatus()) && !Constants.WASTE_BACK.equals(detail.getStatus())) {
+                                    detail.setStatus(Constants.WASTE_PASS);
+                                    detail.setBack_reason(null);
+                                    detail.setBack_reason_index(null);
+                                    detail.setTransfer_time(DateUtils.getCurrentTime1());
+                                }
+                            }
+                        } else {
+                            for (TransferDetail detail : listData) {
+                                if (!Constants.WASTE_PASS.equals(detail.getStatus()) && !Constants.WASTE_BACK.equals(detail.getStatus())) {
+                                    detail.setStatus(Constants.WASTE_BACK);
+                                    detail.setContainer_label_code(null);
+                                    detail.setBack_reason(reasonArray[reasonArray.length - 1]);
+                                    detail.setBack_reason_index(String.valueOf(reasonArray.length - 1));
+                                    detail.setTransfer_time(DateUtils.getCurrentTime1());
+                                }
+                            }
+                        }
+                        break;
+                    case R.id.back_layout:
+                        if (item.getItemId() == R.id.allPass) {
+                            for (TransferDetail detail : listData) {
+                                if (Constants.WASTE_BACK.equals(detail.getStatus())) {
+                                    detail.setStatus(Constants.WASTE_PASS);
+                                    detail.setBack_reason(null);
+                                    detail.setBack_reason_index(null);
+                                    detail.setTransfer_time(DateUtils.getCurrentTime1());
+                                }
+                            }
+                        }
+                        break;
+                }
+                Constants.AFFIRM_LIST.getCollection().get(applyIndex).setModify(true);
+                PreferencesUtils.putString(context, "affirm", new Gson().toJson(Constants.AFFIRM_LIST));
+                PreferencesUtils.putBoolean(context, "isSync", false);
+                updateView();
+                return true;
+            }
+        });
+        popup.show();
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         updateView();
@@ -156,7 +259,12 @@ public class AffirmItemsActivity extends BaseActivity {
 
     @Override
     public void onValidationSucceeded() {
-        int j=0;
+        if (TextUtils.isEmpty(receiver.getText().toString().trim()) && Constants.TRANSFER_TYPE_INNER.equals(getIntent().getStringExtra("type"))) {
+            Toast.makeText(context, "转移人不能为空!", Toast.LENGTH_SHORT).show();
+            receiver.setError("转移人不能为空!");
+            return;
+        }
+        int j = 0;
         for (int i = 0; i < list.size(); i++) {
             if (barCodeData.equals(list.get(i).getLabel_code())) {
                 cacheWasteDialogFragment = CacheWasteDialogFragment.newInstance(applyIndex, i);
@@ -174,7 +282,7 @@ public class AffirmItemsActivity extends BaseActivity {
     @Override
     public void onValidationFailed(List<ValidationError> errors) {
         super.onValidationFailed(errors);
-        Toast.makeText(context, "操作(接收)人不能为空!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "操作人不能为空!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
